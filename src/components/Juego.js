7c3b9183ff3da4f2d5071b932c5b6482c6c8c2dc
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-03 14:31:06 -0500

    Final code update

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -1,96 +1,95 @@
-import { useContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { PALABROS } from "../assets/palabros"
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { PALABROS } from "../assets/palabros";
 import Contexto from "../contexto/Contexto";
 
 const Juego = () => {
-
-  const navegacion = useNavigate ();
-  const letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-  const misColores = [{backgroundColor:"white"}, {backgroundColor:"green", 
-  color:"white"}, {backgroundColor:"red", color:"white"}]
-  const letras_array = letras.split ("");
-  const {escribirCorrecta} = useContext (Contexto);
-  const [azar, setAzar] = useState (0);
-  const [palabra, setPalabra] = useState ([]);
-  const [misLetras,setMisLetras] = useState ([]);
-  const [correctas,setCorrectas] = useState ([]);
-  const [incorrectas,setIncorrectas] = useState ([]);
-  const [imagen, setImagen] = useState (1)
-
-  useEffect(() => {
-    setAzar(Math.floor(Math.random()*PALABROS.length))
-  }, [])
+  const navegacion = useNavigate();
+  const letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  const misColores = [
+    { backgroundColor: "white" },
+    { backgroundColor: "green", color: "white" },
+    { backgroundColor: "red", color: "white" }
+  ];
+  const letras_array = letras.split("");
+  const { escribirCorrecta } = useContext(Contexto);
+  const [azar, setAzar] = useState(0);
+  const [palabra, setPalabra] = useState([]);
+  const [misLetras, setMisLetras] = useState([]);
+  const [correctas, setCorrectas] = useState([]);
+  const [incorrectas, setIncorrectas] = useState([]);
+  const [imagen, setImagen] = useState(1);
 
   useEffect(() => {
-    setPalabra(PALABROS[azar].palabro.split(""))
-    escribirCorrecta(PALABROS[azar].palabro)
-  },[azar])
+    setAzar(Math.floor(Math.random() * PALABROS.length));
+  }, []);
 
-  const presionado = (e)=> {
+  useEffect(() => {
+    setPalabra(PALABROS[azar].palabro.split(""));
+    escribirCorrecta(PALABROS[azar].palabro);
+  }, [azar, escribirCorrecta]);
 
-    const letra= e.target.innerHTML;
-    setMisLetras([...misLetras,(letra)])
-    if (palabra.indexOf(letra) >= 0){
-      setCorrectas([...correctas,(letra)])
+  const presionado = (e) => {
+    const letra = e.target.innerHTML;
+    setMisLetras([...misLetras, letra]);
+    if (palabra.indexOf(letra) >= 0) {
+      setCorrectas([...correctas, letra]);
     } else {
-      setIncorrectas([...incorrectas,(letra)])
-      setImagen(imagen+1)
-      if (imagen>5) {
-        navegacion("/final")
+      setIncorrectas([...incorrectas, letra]);
+      setImagen(imagen + 1);
+      if (imagen > 5) {
+        navegacion("/final");
       }
     }
-  }
-  useEffect(()=>{
-    let noEncontrado=0;
-    palabra.map(p => {
-      if (misLetras.find(e=>e===p)===undefined){
+  };
+
+  useEffect(() => {
+    let noEncontrado = 0;
+    palabra.map((p) => {
+      if (misLetras.find((e) => e === p) === undefined) {
         noEncontrado++;
       }
-    })
-    if (noEncontrado===0 && correctas.length>0){
-      navegacion("/ganado ")
-
+      return null; // Agrega un retorno explícito en el callback de map
+    });
+    if (noEncontrado === 0 && correctas.length > 0) {
+      navegacion("/ganado");
     }
-  }, [correctas])
+  }, [correctas, misLetras, navegacion, palabra]);
 
   return (
     <>
-    <div className="pregunta">
-      {PALABROS[azar].pregunta}
-    </div>
-    <div className="palabra">
-      {
-        palabra.map((letra, i)=>(
-          misLetras.indexOf(letra)===-1
-          ?
+      <div className="pregunta">{PALABROS[azar].pregunta}</div>
+      <div className="palabra">
+        {palabra.map((letra, i) =>
+          misLetras.indexOf(letra) === -1 ? (
             <div className="palo" key={i}></div>
-          :
+          ) : (
             <div className="palo" key={i}>{letra.toUpperCase()}</div>
-        ))
-      }
-    </div>
-    <div className="botones">
-      {
-        letras_array.map((letra) =>(
-          (correctas.find(e=>e===letra))
-          ?
-          <button style={misColores[1]} key={letra}>{letra}</button>
-          :
-          (incorrectas.find(e=>e===letra))
-          ?
-          <button style={misColores[2]} key={letra}>{letra}</button>
-          :
-          <button style={misColores[0]} key={letra} onClick={presionado}>
-          {letra}</button>
-        ))
-      }
-    </div>
-    <div className="imagen">
-      <img src={require(`../assets/el_ahorcado${imagen}.png`)} alt="" />
-    </div>
+          )
+        )}
+      </div>
+      <div className="botones">
+        {letras_array.map((letra) =>
+          correctas.find((e) => e === letra) ? (
+            <button style={misColores[1]} key={letra}>
+              {letra}
+            </button>
+          ) : incorrectas.find((e) => e === letra) ? (
+            <button style={misColores[2]} key={letra}>
+              {letra}
+            </button>
+          ) : (
+            <button style={misColores[0]} key={letra} onClick={presionado}>
+              {letra}
+            </button>
+          )
+        )}
+      </div>
+      <div className="imagen">
+        <img src={require(`../assets/el_ahorcado${imagen}.png`)} alt="" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Juego
+export default Juego;
